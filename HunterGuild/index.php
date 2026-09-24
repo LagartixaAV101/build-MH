42a84484bb6841c17abb02e8bb5e5061b971784b
LagartixaAV101 <108137879+LagartixaAV101@users.noreply.github.com>
2026-09-23 23:50:08 -0300

    index atualizado
    decidi um nome melhor pro projeto e comentei umas coisas pra eu não esquecer depois.
    
    vou passar um tempo matutando um design melhor pro projeto, mas a principio vai ser algo parecido com oq ta por agora

--- HunterGuild/index.php
+++ HunterGuild/index.php
@@ -3,78 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Nome Site</title>
-
+    <title>Monky's Builder</title> <!--editar imagem pra por de icon-->
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-
     <header>
-        <h1>Nome Site</h1>
-
+        <h1>Monky's</h1> <!--procurar fonte legal-->
         <nav>
-            <a href="#">label</a>
-            <a href="#">label</a>
-            <a href="#">label</a>
-
+            <a href="#">Minhas Builds</a>
+            <a href="#">     </a>
+            <a href="#">Comunidade</a>
             <button id="abrirLogin">Entrar</button>
         </nav>
     </header>
-
     <main>
         <h2>Bem-vindo!</h2>
-
         <p>
          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industrys standard dummy text ever since  
-       </p>
-
+         Lorem Ipsum has been the industrys standard dummy text ever since  
+        </p>
         <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industrys standard dummy text ever since 
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+         Lorem Ipsum has been the industrys standard dummy text ever since 
         </p>
     </main>
 
-
     <!-- Fundo escuro do modal -->
     <div id="modalLogin" class="modal">
-
         <!-- Caixa de login -->
         <div class="login-box">
-
             <button id="fecharLogin" class="fechar">&times;</button>
-
             <h2>Entrar</h2>
-
             <form action="login.php" method="POST">
-
                 <label for="email">E-mail</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    required
-                >
+                <input type="email" id="email" name="email" required>
 
                 <label for="senha">Senha</label>
                 <input type="password" id="senha" name="senha" required>
                 <button type="submit" class="botao-login">Entrar</button>
-
             </form>
-
             <p class="cadastro">
                 Ainda não possui uma conta?
                 <a href="#">Criar conta</a>
             </p>
-
         </div>
-
     </div>
 
-
     <script src="script.js"></script>
-
 </body>
 </html>
